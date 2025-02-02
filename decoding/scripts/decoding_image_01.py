765db42c07a2f01056bea9505899f0d4a078fe65
Denyse Umwari <denyseumwari850@gmail.com>
2025-02-02 21:59:52 +0200

    Added barcode recognition and decoding for QR, PDF417, Data Matrix, MaxiCode, and Aztec codes

---
+++ decoding/scripts/decoding_image_01.py
@@ -0,0 +1,6 @@
+from aspose.barcode import barcoderecognition
+reader = barcoderecognition.BarCodeReader(
+    "../images/source/image_01.png", barcoderecognition.DecodeType.ALL_SUPPORTED_TYPES)
+recognized_results = reader.read_bar_codes()
+for barcode in recognized_results:
+    print(barcode.code_text)
